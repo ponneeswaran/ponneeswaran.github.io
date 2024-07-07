a3b81d37fd6f19d5c9b0bb55201e6ed439338029
Ponneeswaran Natarajan <pnatara8@asu.edu>
2024-07-07 20:24:49 +0530

    reverting run deploy

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <>
+        <Header/>
+        {/* <Nav/>
+        <About/>
+        <Experience/>
+        <Myprojects/>
+        <Contacts/>
+        <Footer/> */}
+
+    </>
+  );
+}
+
+export default App;
