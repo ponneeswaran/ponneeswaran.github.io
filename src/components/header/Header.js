cb33a2702dbc7159ec4f09a34133212b259487db
Ponneeswaran Natarajan <pnatara8@asu.edu>
2024-07-08 23:38:46 +0530

    Customization

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,32 @@
 import React from 'react'
-import './header.css'
 import CTA from './CTA'
 import HeaderSocials from './HeaderSocials'
-import { Col, Row, Container, Image } from 'react-bootstrap/esm';
+import { Col, Row, Container } from 'react-bootstrap/esm';
 import 'bootstrap/dist/css/bootstrap.css';
+import './header.css';
+import '../common/fade.css';
 
 
 const Header = () => {
   return (    
-      <header id='home'>
-        <div className="container header__container">
-          <Container>
-            
-            <h5>Hello, I'm</h5>
-            <h1>Ponneeswaran Natarajan</h1>
-            <h5 className="text-light">Senior Software Lead</h5>
-            <CTA/>
-            <HeaderSocials/>
-          </Container>
-        </div>
-      </header>   
+    <Container>
+      <Row>
+        <Col>
+          <header id='home'>
+            <div className="container header__container">
+              <Container>
+                
+                <h5>Hello, I'm</h5>
+                <h1>Ponneeswaran Natarajan</h1>
+                <h5 className="text-light">Senior Software Lead</h5>
+                <CTA/>
+                <HeaderSocials/>
+              </Container>
+            </div>
+          </header> 
+        </Col>
+      </Row>  
+    </Container>
   )
 }
 
